b9aa56b269a65fbf3197c6f236fce296d0bab8c7
gavyen <gavyen.r.kelly@gmail.com>
2025-03-05 14:31:17 -0600

    Final uploads for this phase of project.

--- script.js
+++ script.js
@@ -7,14 +7,24 @@ const newTaskButton = document.getElementById("openNewTaskMenuButton")
 const completedTaskList = document.getElementById("completedTasks")
 const taskPopupPage = document.getElementById("newTaskPopup")
 const tasks = document.getElementById('tasks')
-
+const tabAllTasks = document.getElementById('')
 let numOfTasks = 0
-const addTask = () => {
+
+window.addEventListener('DOMContentLoaded', () => {
+    addTask('Hello', '3/4/2025')
+    addTask('Hello2', '3/4/2025')
+    addTask('Hello3', '3/4/2025')
+})
+
+const submitTaskInfo = () => {
     let newTaskInput = taskInput.value
     let newTaskDueDate = taskDueDate.value
+    return addTask(newTaskInput, newTaskDueDate)
+}
+const addTask = (taskInput, taskDueDate) => {
 
     let newTask = document.createElement('li');
-    newTask.textContent = `${newTaskInput} Due:${newTaskDueDate}`;
+    newTask.textContent = `${taskInput} Due:${taskDueDate}`;
     newTask.classList.add("task")
     numOfTasks++;
     newTask.id = numOfTasks;
@@ -64,7 +74,7 @@ const completeTask = (taskId, taskIdBox) => {
 const toggleTaskInputPopup = () => {
     taskPopupPage.classList.remove('hidden')
 }
-const overlay = popupContainer.querySelector('.overlay');
+const overlay = taskPopupPage.querySelector('.overlay');
 overlay.addEventListener('click', (e) => {
     e.stopPropagation();
 });
